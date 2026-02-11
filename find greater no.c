855d4e002821f8ba774de0b739b1cbc139d89b1d
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-02-11 16:00:58 +0530

    Add files via upload

---
+++ find greater no.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("%d is greater",a);
+    }else if(a<b){
+        printf("%d is greater",b);
+    }else{
+        printf("a and b is equal");
+    }
+    return 0;
+}
