7db654c10194a334529fbcd16c6d140f692340c2
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-02-12 09:22:36 +0530

    Add files via upload

---
+++ Voting Eligibility .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=18){
+        printf("Eligible to vote");
+    }
+    else{
+        printf("Not Eligible to vote");
+    }
+   return 0;
+}
