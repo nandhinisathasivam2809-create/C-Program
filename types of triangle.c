028215faf97b73a1af86bd31d7426873e5fc6698
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-02-12 10:54:17 +0530

    Add files via upload

---
+++ types of triangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a==b && b==c){
+        printf("equilateral triangle");
+    }
+    else if(a==b || b==c || a==c){
+        printf("Isosceles triangle");
+    }
+    else
+        printf("scalene triangle");
+   return 0;
+}
