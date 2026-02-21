3690b8d4fc218609cc53f634c01ac1725940c8ec
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-02-21 11:22:20 +0530

    Add files via upload

---
+++ online order cancellation risk monitor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int N, i = 0;
+    scanf("%d", &N);
+
+    int order, success = 0, cancel = 0;
+
+    while(i < N) {
+        scanf("%d", &order);
+
+        if(order == 1)
+            success++;
+        else
+            cancel++;
+
+        i++;
+    }
+
+    printf("Successful: %d\n", success);
+    printf("Cancelled: %d\n", cancel);
+
+    if(cancel > success)
+        printf("Status: Risk\n");
+    else
+        printf("Status: Safe\n");
+
+    return 0;
+}
