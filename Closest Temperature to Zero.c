a80c0bd0da247568df049b69a6f583a8faac6b99
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-05 14:32:24 +0530

    Add files via upload

---
+++ Closest Temperature to Zero.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int closest = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(abs(arr[i]) < abs(closest) || 
+          (abs(arr[i]) == abs(closest) && arr[i] > closest))
+            closest = arr[i];
+    }
+
+    printf("%d", closest);
+    return 0;
+}
