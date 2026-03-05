a9c3980e5b87c9462b3842560c1bb1c94fc5b2d5
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-05 11:44:12 +0530

    Add files via upload

---
+++ replace with future mximum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+        
+    int max=-1,temp;
+    
+    for(int i=n-1;i>=0;i--){
+        temp=arr[i];
+        arr[i]=max;
+        if(temp>max)
+            max=temp;
+    }
+    
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+        
+    return 0;
+}
