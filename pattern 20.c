82afe3fcd2af9813d765fab7340300a6b37c805e
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-05 16:10:49 +0530

    Add files via upload

---
+++ pattern 20.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+
+        for(j=1;j<=2*(n-i);j++)
+        {
+            printf(" ");
+        }
+
+        for(j=i;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
