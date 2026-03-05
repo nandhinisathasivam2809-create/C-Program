2c6e1d5fa766be3a88cba0f65f96dc12172695eb
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-05 11:40:55 +0530

    Add files via upload

---
+++ remove duplicate customer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        int duplicate = 0;
+
+        for(int j = 0; j < i; j++) {
+            if(arr[i] == arr[j]) {
+                duplicate = 1;
+                break;
+            }
+        }
+
+        if(duplicate == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
