76c9fad2c518dfd011ba96a726c92f661b45b822
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-05 14:26:39 +0530

    Add files via upload

---
+++ Count Unique Visitors.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(int i = 0; i < n; i++) {
+        int flag = 0;
+        for(int j = 0; j < i; j++) {
+            if(arr[i] == arr[j]) {
+                flag = 1;
+                break;
+            }
+        }
+        if(!flag)
+            count++;
+    }
+
+    printf("%d", count);
+    return 0;
+}
