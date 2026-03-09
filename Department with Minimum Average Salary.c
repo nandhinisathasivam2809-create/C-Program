2a59efc1c8a7a730fa705c07d8d3d72082de984a
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-09 11:53:24 +0530

    Add files via upload

---
+++ Department with Minimum Average Salary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int R,C;
+    scanf("%d%d",&R,&C);
+    int a[R][C];
+
+    for(int i=0;i<R;i++)
+        for(int j=0;j<C;j++)
+            scanf("%d",&a[i][j]);
+
+    int minSum=1000000000,index=0;
+
+    for(int i=0;i<R;i++){
+        int sum=0;
+        for(int j=0;j<C;j++)
+            sum+=a[i][j];
+
+        if(sum<minSum){
+            minSum=sum;
+            index=i;
+        }
+    }
+
+    printf("%d",index);
+}
