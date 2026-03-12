0b5f0e49eece08ab5771a81eb35d9832bd729f6f
nandhinisathasivam2809-create <nandhinisathasivam2809@gmail.com>
2026-03-12 09:54:40 +0530

    Add files via upload

---
+++ Consonant Counter.C
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, count = 0;
+
+    scanf("%s", str);
+
+    while(str[i] != '\0') {
+        char c = str[i];
+
+        if((c>='A'&&c<='Z')||(c>='a'&&c<='z')) {
+            if(!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+                 c=='A'||c=='E'||c=='I'||c=='O'||c=='U'))
+                count++;
+        }
+        i++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
